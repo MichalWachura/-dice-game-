f7df2142446d097dc562b12a90ec9acbd7a2ab1f
Michał <130533795+MichalWachura@users.noreply.github.com>
2023-07-08 00:29:55 +0200

    Add files via upload
    Fix labels updateing,

--- app.js
+++ app.js
@@ -5,26 +5,54 @@ let scoreTwo = document.querySelector('.score-2 p');
 
 
 
-
-let buttonOne = document.querySelector(".score-1.button");
-let buttonTwo = document.querySelector(".score-2.button");
-
-
-
-let displayScoreOne = scoreOne.textContent = randomNumber1;
-let displayScoreTwo = scoreTwo.textContent = randomNumber2;
+let buttonOne = document.querySelector(".score-1 button");
+let buttonTwo = document.querySelector(".score-2 button");
+let score1 = 0;
+let score2 = 0;
 
 
+buttonOne.addEventListener("click", drawNumber1);
+buttonOne.addEventListener("click", titleUpdate );
 
+
+buttonTwo.addEventListener("click", drawNumber2);
+buttonTwo.addEventListener("click",titleUpdate);
+
+
+
+
+
+
+
+function titleUpdate (){
+    if( score1 === score2 ){
+      title.textContent = " ! MYSZEK !";
+    }
+    else if( score1 < score2 ){
+      title.textContent = " Michał WINS!";
+    }else{
+      title.textContent = "Dominisia WINS!";
+    }};
 
 function drawNumber1(){
     let randomNumber1 = Math.floor(Math.random()*6 +1);
-    return randomNumber1;
+    let displayScoreOne = scoreOne.textContent = randomNumber1;
+    score1 = displayScoreOne;
+    return score1;
+    
 };
 function drawNumber2(){
     let randomNumber2 = Math.floor(Math.random()*6 +1);
-    return randomNumber2;
+    let displayScoreTwo = scoreTwo.textContent = randomNumber2;
+    score2 = displayScoreTwo;
+    return score2;
 };
 
-buttonOne.addEventListener("click", drawNumber1);
-buttonTwo.addEventListener("click", drawNumber2);
+
+
+
+
+
+
+
+
